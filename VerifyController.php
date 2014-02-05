e20afb2ce2af9d85de2a919169356485468e054d
nitinkumarmedia <nitin.kumar@mediaagility.com>
2014-02-05 11:37:36 +0530

    Test file for demo

---
+++ VerifyController.php
@@ -0,0 +1,21 @@
+<?php
+/*  VerifyController - Used to handle multiple request on the server through GET/POST/REQUEST
+ *  Author: Nitin
+ *  Date: 11-Jan-2013
+ *  Version: 1.7.2.1
+ */ 
+class VerifyController
+{
+    public $postVars;
+    public $getVars;
+    public $response;
+   
+    public function __construct($get, $post)
+    {
+        $this->postVars = $post;
+	$this->getVars  = $get;
+        $ObjModel = new VerifyModel($this->getVars, $this->postVars);
+        $this->response = $ObjModel->response;
+    }
+}
+?>
